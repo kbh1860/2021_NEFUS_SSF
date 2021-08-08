f11d0dca6f093f042ec5f390314b61994c41bde9
rolling <kbh10211@gmail.com>
2021-08-09 00:24:20 +0900

    modify

--- Sticker.py
+++ Sticker.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtGui import QMovie
+from PyQt5.QtGui import QMovie #Show GIF FIle
 
 class Sticker(QtWidgets.QMainWindow):
     def __init__(self, img_path, xy, size=1.0, on_top=False):
         super(Sticker, self).__init__()
+
         self.timer = QtCore.QTimer(self)
         self.img_path = img_path
         self.xy = xy
@@ -20,6 +21,44 @@ class Sticker(QtWidgets.QMainWindow):
 
         self.setupUi()
         self.show()
+
+    def setupUi(self):
+        centralWidget = QtWidgets.QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint if self.on_top else QtCore.Qt.FramelessWindowHint)
+        #FramlessWindowHint = Remove FramelessWindowHint
+        #if object's location is on top -> WindowStaysOnTopHint
+        #else FramlessWindowHint
+        self.setWindowFlags(flags)
+        #Settings Save
+        self.setAttribute(QtCore.Qt.WA_NoSystemBackground, True)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
+        #NoSystemBackground, TranslucentBackground -> transparent
+
+        label = QtWidgets.QLabel(centralWidget)
+            #Make The loation for label
+
+        movie = QMovie(self.img_path)
+            #loading the GIF file
+        label.setMovie(movie)
+            #GIF Setting
+        movie.start()
+        #Start GIF
+        movie.stop()
+        #Stop GIF
+
+        w = int(movie.frameRect().size().width() * self.size)
+        #width
+        h = int(movie.frameRect().size().height() * self.size)
+        #height
+        movie.setScaledSize(QtCore.QSize(w, h))
+        #Set the GIF file
+
+        movie.start()
+
+        self.setGeometry(self.xy[0], self.xy[1], w, h)
+        #GIF window size location
 
     # 마우스 놓았을 때
     def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
@@ -40,8 +79,11 @@ class Sticker(QtWidgets.QMainWindow):
 
     def walk(self, from_xy, to_xy, speed=60):
         self.from_xy = from_xy
+        #GIF source 
         self.to_xy = to_xy
+        #GIF Destination
         self.speed = speed
+        #GIF speed
 
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.__walkHandler)
@@ -63,50 +105,34 @@ class Sticker(QtWidgets.QMainWindow):
     def __walkHandler(self):
         if self.xy[0] >= self.to_xy[0]:
             self.direction[0] = 0
+            #set the location by left
         elif self.xy[0] < self.from_xy[0]:
             self.direction[0] = 1
+            #set the location by right
 
         if self.direction[0] == 0:
             self.xy[0] -= 1
+            #move to left
         else:
             self.xy[0] += 1
+            #move to right
 
         if self.xy[1] >= self.to_xy[1]:
             self.direction[1] = 0
+            #set the location by left
         elif self.xy[1] < self.from_xy[1]:
             self.direction[1] = 1
+            #set the location by right
 
         if self.direction[1] == 0:
             self.xy[1] -= 1
+            #move to left
         else:
             self.xy[1] += 1
+            #move to right
 
         self.move(*self.xy)
-
-    def setupUi(self):
-        centralWidget = QtWidgets.QWidget(self)
-        self.setCentralWidget(centralWidget)
-
-        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint if self.on_top else QtCore.Qt.FramelessWindowHint)
-        self.setWindowFlags(flags)
-        self.setAttribute(QtCore.Qt.WA_NoSystemBackground, True)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
-
-        label = QtWidgets.QLabel(centralWidget)
-        movie = QMovie(self.img_path)
-        label.setMovie(movie)
-        movie.start()
-        movie.stop()
-
-        w = int(movie.frameRect().size().width() * self.size)
-        h = int(movie.frameRect().size().height() * self.size)
-        movie.setScaledSize(QtCore.QSize(w, h))
-        movie.start()
-
-        self.setGeometry(self.xy[0], self.xy[1], w, h)
-
-    def mouseDoubleClickEvent(self, e):
-        QtWidgets.qApp.quit()
+        #Move!
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
@@ -121,12 +147,12 @@ if __name__ == '__main__':
     s3 = Sticker('./amongus/blue_green.gif', xy=[400, 920], size=1.0, on_top=True)
 
     s4 = Sticker('./amongus/mint.gif', xy=[1000, 950], size=0.2, on_top=True)
-    s4.walk_diff(from_xy_diff=[-100, 0], to_xy_diff=[100, 0], speed=120)
+    # s4.walk_diff(from_xy_diff=[-100, 0], to_xy_diff=[100, 0], speed=120)
 
     s5 = Sticker('./amongus/brown.gif', xy=[200, 1010], size=0.75, on_top=True)
 
     s6 = Sticker('./amongus/yellow.gif', xy=[1850, 800], size=0.75, on_top=True)
-    # s6.walk(from_xy=[0, 800], to_xy=[1850, 800], speed=240)
+    s6.walk(from_xy=[0, 800], to_xy=[1850, 800], speed=240)
 
     s7 = Sticker('./amongus/magenta.gif', xy=[1500, 900], size=0.5, on_top=True)
     s7.walk_diff(from_xy_diff=[-100, 0], to_xy_diff=[100, 0], speed=180)
